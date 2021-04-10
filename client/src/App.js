2ed99b42f8f25f3a317169bb0f21ef19037dcb0e
Troy Dorman <troydorman@gmail.com>
2021-04-10 16:26:51 -0600

    added ability to display single post when clicked on home page of app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from './Nav';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 const App = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    axios.get(`${process.env.REACT_APP_API}/posts`)
+    .then(response => {
+      console.log(response)
+      setPosts(response.data)
+    })
+    .catch(error => alert('Error fetching posts'));
+    
+  }
+
+  useEffect(() => {
+    fetchPosts();
+
+  }, [])
+
   return (
     <div className="container pb-5">
       <Nav/>
       <br/>
       <h1> MERN CRUD </h1>
+      <hr/>
+   {/*    {JSON.stringify(posts)} */}
+      {
+        posts.map((post, i) => (
+          <div className='row' key={post._id} style={{borderBottom: '1px solid silver'}}>
+             <div className="col pt-3 pb-2">
+               
+               <Link to={`/post/${post.slug}`}>
+                 <h2>{post.title}</h2>
+               </Link>
+              
+               <p className="lead">{post.content.substring(0,100)}</p>
+
+               <p>
+                 Author <span className="badge">{post.user}</span> Published on 
+                 <span className="badge">{new Date(post.createdAt).toLocaleString()}</span>
+                 
+              </p>
+              
+             </div>
+          </div>
+        ))
+      }
     </div>
   );
 };
