8714d76637fae607614a048e42d76195458fe277
Troy Dorman <troydorman@gmail.com>
2021-04-10 11:59:48 -0600

    successfully writing new posts to the database via Postman

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,5 +1,31 @@
+const Post = require('../models/post');
+const slugify = require('slugify');
+
+
 exports.create = (req, res) => {
-    res.json({
-        data: 'hello from controllers create method'
+    // console.log(req.body);
+    const {title, content, user} = req.body
+    const slug = slugify(title)
+    
+    // validate
+
+    switch(true) {
+        case !title:
+        return res.status(400).json({error: 'title is required'});
+        case !content:
+        return res.status(400).json({error: 'content is required'});
+    }
+    
+    Post.create({title, content, user, slug}, (err, post) => {
+        if(err){
+            console.log(err)
+            res.status(400).json({error: 'duplicate post. Try another title.'})
+        }
+
+        res.json(post);
+
     })
+
+
+    
   };
--- server/routes/post.js
+++ server/routes/post.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { create } = require('../controllers/post');
 
 
-router.get('/post', create);
+router.post('/post', create);
 
 
 
