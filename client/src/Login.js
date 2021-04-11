d0a7ff7e3da793aa4d3aeb5252c4a489748cfa08
Troy Dorman <troydorman@gmail.com>
2021-04-11 14:18:28 -0600

    wrote helper functions to facilitate storing reading and removing our authorization tokens from session storage

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import Nav from './Nav';
+import {authenticate} from './helpers';
 
 
-const Login = () => {
+
+const Login = (props) => {
 
     const [state, setState] = useState({
         name: '',
@@ -27,6 +29,7 @@ const Login = () => {
               
               console.log(response)
               // response will contain token and name
+              authenticate(response, () => props.history.push('/create'));
               // redirect to create page
               
     
@@ -65,4 +68,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default withRouter(Login)
