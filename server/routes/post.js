93546bccb9f67c835fbb943c47cb0c17f17deb30
Troy Dorman <troydorman@gmail.com>
2021-04-12 14:37:36 -0600

    trying to deploy. this is always fun.

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,15 +3,21 @@ const router = express.Router();
 
 // import controller methods
 const { create, list, read, update, remove } = require('../controllers/post');
+const {requireSignin} = require('../controllers/auth');
 
 
-router.post('/post', create);
+router.post('/post', requireSignin, create);
 router.get('/posts', list);
 router.get('/post/:slug', read);
-router.put('/post/:slug', update);
-router.delete('/post/:slug', remove);
+router.put('/post/:slug', requireSignin, update);
+router.delete('/post/:slug', requireSignin, remove);
 
 
+router.get('/secret', requireSignin, (req,res) => {
+    res.json({
+        data: req.user.name
+    })
+})
 
 
 
